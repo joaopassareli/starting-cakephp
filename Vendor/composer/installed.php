f0e5eb84dcc58dfb8052a5ba91bef3dab216794a
João Passareli <joaopassareli@gmail.com>
2022-09-22 13:03:01 -0300

    Subindo o projeto inicial no git

---
+++ Vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'cakephp/cakephp' => array(
+            'pretty_version' => '2.10.22',
+            'version' => '2.10.22.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cakephp/cakephp',
+            'aliases' => array(),
+            'reference' => 'd0c51b37422d0d2c99be74045d4439a674259308',
+            'dev_requirement' => false,
+        ),
+    ),
+);
